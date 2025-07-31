89cecc487d1d93e9ea346d5f55f4b0860304facb
Antwi Kwarteng-Amaning <mailantwi@icloud.com>
2025-07-31 14:42:57 +0000

    Release: v0.2.14

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-settings',
-    version='0.2.13',
-    packages=find_packages(),
+    version='0.2.14',
+    packages=find_packages(include=["django_settings", "django_settings.*"]),
     include_package_data=True,
     install_requires=[
         'Django>=3.2',
