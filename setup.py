65d4a985590be8ea611a69f23133b781e72209b4
Antwi Kwarteng-Amaning <mailantwi@icloud.com>
2025-07-31 13:09:01 +0000

    Release: v0.2.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-settings',
-    version='0.2.11',
+    version='0.2.13',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
